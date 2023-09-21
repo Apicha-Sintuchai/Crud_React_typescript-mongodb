5e3fd2d89763e391009ec608960872781aaaf453
apicha <apichai2556anet@gmail.com>
2023-09-21 23:43:43 +0700

    first commit

---
+++ backend/mongotable/mongoshema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const BookSchema = mongoose.Schema({
+    Bookname:{
+        type:String
+    },
+    Bookdetail:{
+        type:String
+    },
+    BookPrice:{
+        type:Number
+    },
+    file: String,
+    shotstory:{
+        type:String
+    }
+    
+    // BookUser:{
+    //     type:String,
+    //     default:'user'
+    // }
+}, { timestamps: true })
+
+module.exports = mongoose.model('BookShema',BookSchema)
